312698eefe1288f27a084d6efb30810eb8980477
Adam Wenham <adamwenham64@gmail.com>
2021-08-03 22:51:50 +0200

    import mock data as config

--- config/config.exs
+++ config/config.exs
@@ -26,3 +26,6 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
+
+# Import mock data for use in API responses
+import_config "mock_data.exs"
