e94aa66492299120ff69b30411e981e497543397
Adam Wenham <adamwenham64@gmail.com>
2021-08-07 16:25:10 +0200

    move accounts generation test from accounts controller test to data generator test

--- test/teller_sandbox/accounts_test.exs
+++ test/teller_sandbox/accounts_test.exs
@@ -3,58 +3,6 @@ defmodule TellerSandbox.AccountsTest do
 
   alias TellerSandbox.Accounts
 
-  describe "list_accounts/1 " do
-    test "it returns all accounts" do
-      assert Accounts.list_accounts("test_1234567") == [
-               %{
-                 account_number: 367,
-                 balances: %{available: "93.60", ledger: "93.60"},
-                 currency_code: "USD",
-                 enrollment_id: "test_-7YND7I9QZGpt2f",
-                 id: "test_W56xjgDUFBFqjKZ",
-                 institution: %{id: "wells_fargo", name: "Wells Fargo"},
-                 links: %{
-                   self: "http://localhost:4000/accounts/test_W56xjgDUFBFqjKZ",
-                   transactions:
-                     "http://localhost:4000/accounts/test_W56xjgDUFBFqjKZ/transactions"
-                 },
-                 name: "Test Checking Account",
-                 routing_numbers: %{ach: "615933022", wire: "469420120"}
-               },
-               %{
-                 account_number: 367,
-                 balances: %{available: "80.11", ledger: "80.11"},
-                 currency_code: "USD",
-                 enrollment_id: "test_J20MZ2pjKyd_bTJ",
-                 id: "test_rdt7AusLY2CTPG_",
-                 institution: %{id: "capital_one", name: "Capital One"},
-                 links: %{
-                   self: "http://localhost:4000/accounts/test_rdt7AusLY2CTPG_",
-                   transactions:
-                     "http://localhost:4000/accounts/test_rdt7AusLY2CTPG_/transactions"
-                 },
-                 name: "Test Checking Account",
-                 routing_numbers: %{ach: "176225235", wire: "354097347"}
-               },
-               %{
-                 account_number: 367,
-                 balances: %{available: "7.139", ledger: "7.139"},
-                 currency_code: "USD",
-                 enrollment_id: "test_XZSczG6P_KslBaX",
-                 id: "test_5RCuUlRdc4-0PpB",
-                 institution: %{id: "wells_fargo", name: "Wells Fargo"},
-                 links: %{
-                   self: "http://localhost:4000/accounts/test_5RCuUlRdc4-0PpB",
-                   transactions:
-                     "http://localhost:4000/accounts/test_5RCuUlRdc4-0PpB/transactions"
-                 },
-                 name: "Test Checking Account",
-                 routing_numbers: %{ach: "951594080", wire: "294937012"}
-               }
-             ]
-    end
-  end
-
   describe "get_account!/2" do
     test "it returns an account by id" do
       assert Accounts.get_account!("test_W56xjgDUFBFqjKZ", "test_1234567") ==
--- test/teller_sandbox/data_generation/data_generator_test.exs
+++ test/teller_sandbox/data_generation/data_generator_test.exs
@@ -3,6 +3,58 @@ defmodule TellerSandbox.DataGeneration.DataGeneratorTest do
 
   alias TellerSandbox.DataGeneration.DataGenerator
 
+  describe "#generate_accounts/1" do
+    test "returns a pseudorandom collection of accounts" do
+      assert DataGenerator.generate_accounts("test_1234567") == [
+               %{
+                 account_number: 367,
+                 balances: %{available: "93.60", ledger: "93.60"},
+                 currency_code: "USD",
+                 enrollment_id: "test_-7YND7I9QZGpt2f",
+                 id: "test_W56xjgDUFBFqjKZ",
+                 institution: %{id: "wells_fargo", name: "Wells Fargo"},
+                 links: %{
+                   self: "http://localhost:4000/accounts/test_W56xjgDUFBFqjKZ",
+                   transactions:
+                     "http://localhost:4000/accounts/test_W56xjgDUFBFqjKZ/transactions"
+                 },
+                 name: "Test Checking Account",
+                 routing_numbers: %{ach: "615933022", wire: "469420120"}
+               },
+               %{
+                 account_number: 367,
+                 balances: %{available: "80.11", ledger: "80.11"},
+                 currency_code: "USD",
+                 enrollment_id: "test_J20MZ2pjKyd_bTJ",
+                 id: "test_rdt7AusLY2CTPG_",
+                 institution: %{id: "capital_one", name: "Capital One"},
+                 links: %{
+                   self: "http://localhost:4000/accounts/test_rdt7AusLY2CTPG_",
+                   transactions:
+                     "http://localhost:4000/accounts/test_rdt7AusLY2CTPG_/transactions"
+                 },
+                 name: "Test Checking Account",
+                 routing_numbers: %{ach: "176225235", wire: "354097347"}
+               },
+               %{
+                 account_number: 367,
+                 balances: %{available: "7.139", ledger: "7.139"},
+                 currency_code: "USD",
+                 enrollment_id: "test_XZSczG6P_KslBaX",
+                 id: "test_5RCuUlRdc4-0PpB",
+                 institution: %{id: "wells_fargo", name: "Wells Fargo"},
+                 links: %{
+                   self: "http://localhost:4000/accounts/test_5RCuUlRdc4-0PpB",
+                   transactions:
+                     "http://localhost:4000/accounts/test_5RCuUlRdc4-0PpB/transactions"
+                 },
+                 name: "Test Checking Account",
+                 routing_numbers: %{ach: "951594080", wire: "294937012"}
+               }
+             ]
+    end
+  end
+
   describe "#generate_accounts_and_transactions/1" do
     test "returns a pseudorandom collection of accounts and transactions" do
       assert DataGenerator.generate_accounts_and_transactions("test_123456") == [
